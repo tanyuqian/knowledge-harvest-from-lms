2902c42bba32e054f12b0360cef48a4ba78570b0
bwt09 <tanbowen@sjtu.edu.cn>
2022-03-21 23:36:20 -0400

    updated.

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from models.knowledge_harvester import KnowledgeHarvester
 def main():
     knowledge_harvester = KnowledgeHarvester(model_name='roberta-large')
 
+    knowledge_harvester.init_prompts(prompts=['<ENT0> is part of <ENT1>'])
     knowledge_harvester.harvest()
 
 
