ae9211437da68f20c7ac0bf91a3a8dc06f301547
bwt09 <tanbowen@sjtu.edu.cn>
2022-03-23 00:22:58 -0400

    updated.

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from models.knowledge_harvester import KnowledgeHarvester
 def main():
     knowledge_harvester = KnowledgeHarvester(model_name='roberta-large')
 
+    # knowledge_harvester.init_prompts(prompts=[
+    #     '<ENT1>\'s capital is <ENT0>',
+    #     '<ENT0> is <ENT1>\'s capital',
+    #     '<ENT0> is the capital of <ENT1> and one of the most populous cities in the world',
+    #     'the capital of <ENT1> is <ENT0>',
+    #     '<ENT1>\'s capital city is <ENT0>'])
+
     knowledge_harvester.init_prompts(prompts=[
-        '<ENT1>\'s capital is <ENT0>',
-        '<ENT0> is <ENT1>\'s capital',
-        '<ENT0> is the capital of <ENT1> and one of the most populous cities in the world',
-        'the capital of <ENT1> is <ENT0>',
-        '<ENT1>\'s capital city is <ENT0>'])
+        '<ENT0> is part of <ENT1>',
+        '<ENT0> belongs to <ENT1>'])
 
     knowledge_harvester.update_ent_tuples()
 
