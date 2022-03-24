10544b6dd1bbbe7d2c9dc3d5802bf6ce3dcb1a14
bwt09 <tanbowen@sjtu.edu.cn>
2022-03-24 16:38:02 -0400

    updated.

--- main.py
+++ main.py
@@ -5,11 +5,14 @@ def main():
     knowledge_harvester = KnowledgeHarvester(model_name='roberta-large')
 
     knowledge_harvester.init_prompts(prompts=[
-        '<ENT1>\'s capital is <ENT0>',
-        '<ENT0> is <ENT1>\'s capital',
-        '<ENT0> is the capital of <ENT1> and one of the most populous cities in the world',
-        'the capital of <ENT1> is <ENT0>',
-        '<ENT1>\'s capital city is <ENT0>'])
+        '<ENT0> is a subclass of <ENT1>'])
+
+    # knowledge_harvester.init_prompts(prompts=[
+    #     '<ENT1>\'s capital is <ENT0>',
+    #     '<ENT0> is <ENT1>\'s capital',
+    #     '<ENT0> is the capital of <ENT1> and one of the most populous cities in the world',
+    #     'the capital of <ENT1> is <ENT0>',
+    #     '<ENT1>\'s capital city is <ENT0>'])
 
     # knowledge_harvester.init_prompts(prompts=[
     #     '<ENT0> is part of <ENT1>',
