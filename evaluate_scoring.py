13b13982f91a009656ba98611f532be8e4f79d68
Ber666 <ber66666@126.com>
2022-04-07 02:15:36 -0400

    fix: a bug causing multiple prmopts to perform bad is fixed

--- evaluate_scoring.py
+++ evaluate_scoring.py
@@ -11,11 +11,13 @@ import os
 
 def main():
     test_file = "conceptnet_high_quality.txt"
+    # weights = (.25, .25, 1)
+    weights = (0, 1, 0)
     # test_file = 'test.txt'
     ckbc = CKBC(test_file)
     knowledge_harvester = KnowledgeHarvester(
         model_name='roberta-large', max_n_ent_tuples=None)
-    save_dir = f'ckbc_{test_file.split(".")[0]}_curves'
+    save_dir = f'neww_ckbc_{test_file.split(".")[0]}_{weights}_curves'
     os.makedirs(save_dir, exist_ok=True)
     for relation, init_prompts in conceptnet_relation_init_prompts.items():
         if relation not in ckbc._ent_tuples:
@@ -24,10 +26,10 @@ def main():
         n_tuples = len(ckbc.get_ent_tuples(rel=relation))
         for n_prompts in [1, 2, 3]:
             knowledge_harvester.clear()
-            knowledge_harvester.init_prompts(prompts=init_prompts[:n_prompts])
-
+            prompts = init_prompts
+            knowledge_harvester.init_prompts(prompts=prompts[:n_prompts])
             weighted_ent_tuples = knowledge_harvester.get_ent_tuples_weight(
-                ckbc.get_ent_tuples(rel=relation))
+                ckbc.get_ent_tuples(rel=relation), weights=weights)
 
             y_true, scores = [], []
             for ent_tuple, weight in weighted_ent_tuples:
@@ -38,13 +40,13 @@ def main():
 
             precision, recall, _ = precision_recall_curve(y_true, scores)
             plt.plot(recall, precision, label=f'{n_prompts}prompts')
-
+        plt.xlim((0, 1))
+        plt.ylim((0, 1))
         plt.legend()
         plt.title(f"{relation}: {n_tuples} tuples")
 
         plt.savefig(f"{save_dir}/{relation}.png")
         plt.figure().clear()
-
-
+        # 
 if __name__ == '__main__':
     fire.Fire(main)
