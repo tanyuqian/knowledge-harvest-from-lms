35a2f4ef8587b3addbeffaf808d7bdffa8ed3497
Hengzhe Zhang <2970057811@qq.com>
2022-06-16 23:07:23 +0800

    disable cuda

--- server_main.py
+++ server_main.py
@@ -1,5 +1,6 @@
 import json
 import os
+from pathlib import Path
 
 from flask import Flask, jsonify
 
@@ -66,5 +67,6 @@ def predict(model_name, init_prompts_str, seed_ent_tuples_str):
 
 
 if __name__ == '__main__':
+    os.environ['PATH'] = str(Path.home()) + "/anaconda3/bin" + ':' + os.environ['PATH']
     # app.run(host='0.0.0.0', port=1111, debug=True)
     app.run(host='127.0.0.1', port=8000, debug=True)
